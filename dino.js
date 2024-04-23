9062e0d247ab59cab05bdc45bf3a9750516f65f6
Jiraphat Namvong <jiraphatnam2204@gmail.com>
2024-04-23 18:57:14 +0700

    create dino

--- dino.js
+++ dino.js
@@ -3,3 +3,35 @@ let board;
 let boardwidth = 750;
 let boardheight = 250;
 let context;
+
+// dino
+let dinowidth = 88;
+let dinoheight = 94;
+let dinox = 50;
+let dinoy = boardheight - dinoheight;
+let dinoimg;
+
+let dino = {
+    x: dinox,
+    y: dinoy,
+    width: dinowidth,
+    height: dinoheight
+}
+
+// cactus
+
+
+window.onload = function(){
+    board = document.getElementById("board");
+    board.height = boardheight;
+    board.width = boardwidth;
+
+    context = board.getContext("2d");
+    // context.fillStyle="green";
+    // context.fillRect(dino.x, dino.y, dino.width, dino.height);
+    dinoimg = new Image();
+    dinoimg.src = "./img/dino.png";
+    dinoimg.onload = function(){
+        context.drawImage(dinoimg, dino.x, dino.y, dino.width, dino.height);
+    }
+}
